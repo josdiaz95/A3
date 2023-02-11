a3d7acb8f6a7a6fc6ff8a4cfd0466256e8175d5b
Jos <josdiaz95@outlook.com>
2023-02-10 19:05:50 -0600

    Add project files.

---
+++ A3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace A3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime today = DateTime.Now;
+        var myDay = $"{today:dddd}";
+        var myTime = $"{today:hh}:{today:mm}{today:tt}";
+        var myDate = $"{today:MMMM} {today:dd},{today:yyyy}";
+
+
+
+        Console.WriteLine($"1.{myDate,40}");
+        Console.WriteLine($"2.{today:yyyy}.{today:MM}.{today:dd}");
+        Console.WriteLine($"3.Day {today:dd} of {today:MMMM},{today:yyyy}");
+        Console.WriteLine($"4.Year:{today:yyyy},Month:{today:MM},Day:{today:dd}");
+        Console.WriteLine($"5.{myDay,10}");
+        Console.WriteLine($"6.{myTime,10}{myDay,10}");
+        Console.WriteLine($"7.h:{today:hh},m:{today:mm},s:{today:ss}");
+        Console.WriteLine($"8.{today:yyyy}.{today:MM}.{today:dd}.{today:hh}.{today:mm}.{today:ss}");
+        var pi = Math.PI;
+        Console.WriteLine($"{pi:C}");
+        Console.WriteLine($"{pi,10:f3}");
+
+
+    }
+}
